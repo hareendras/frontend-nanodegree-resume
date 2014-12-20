02673120facf202194b0eb4306cf5d9457718a42
Hareendra Seneviratne <hareendras@gmail.com>
2014-12-20 09:54:54 +0530

    Adding contact info

---
+++ js/resumeBuilder.js
@@ -0,0 +1,19 @@
+/*var awesomeThoughts = "I'm hareendra and I'm awesome";
+var funThoughts = awesomeThoughts.replace("awesome","fun");
+$("#main").append(funThoughts);*/
+var bio = {
+  name : "Hareendra Seneviratne",
+  role : "Web Developer",
+  contact : "No 349 Dalupotha Negombo",
+  pictureURL : "#",
+  welcomeMessage : "Welcome ",
+  skills : ["java","javascript","mongo"]
+
+};
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+var formattedContact = HTMLcontactGeneric.replace("%contact%",bio.contact);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedContact);
