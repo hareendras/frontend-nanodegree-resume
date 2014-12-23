c83db7af7fd7ccda77095b60a023c2c6849561ef
Hareendra Seneviratne <hareendras@gmail.com>
2014-12-23 09:56:20 +0530

    adding contact info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,18 +2,52 @@
 var funThoughts = awesomeThoughts.replace("awesome","fun");
 $("#main").append(funThoughts);*/
 var bio = {
-  name : "Hareendra Seneviratne",
-  role : "Web Developer",
-  contact : "No 349 Dalupotha Negombo",
-  pictureURL : "#",
-  welcomeMessage : "Welcome ",
-  skills : ["java","javascript","mongo"]
+  "name" : "Hareendra Seneviratne",
+  "role" : "Web Developer",
+  "contacts" : {
+    "mobile" : "+94715249388",
+    "email" : "hareendras@gmail.com",
+    "github" : "github",
+    "twitter" : "twitter",
+    "location" : "Colombo"
+  },
+  "welcomeMessage" : "Welcome ",
+  "pictureURL" : "#",
+
+  "skills" : ["java","javascript","mongo"],
+  "bioPic" : "#"
+
 
 };
 var formattedName = HTMLheaderName.replace("%data%",bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
-var formattedContact = HTMLcontactGeneric.replace("%contact%",bio.contact);
+//var formattedContact = HTMLcontactGeneric.replace("%contact%",bio.contacts.mobile);
+var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
+var formattedTwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter);
+var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
-$("#header").append(formattedContact);
+//$("#header").append(formattedContact);
+$("#header").append(formattedMobile);
+$("#header").append(formattedEmail);
+/*
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+*/
+
+var work = {
+  "workStart" : "2012-10-12",
+  "workEmployer" : "Millenniumi Information Technologies",
+  "workTitle" : "Implementation engineer",
+  "workDates" : "",
+  "workLocation" : "Colombo",
+  "workDescription" : "implementing Finacle products"
+
+
+};
